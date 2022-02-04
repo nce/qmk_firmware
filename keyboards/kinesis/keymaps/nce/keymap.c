d4c93b76de8308727743be6d34c881d8dae5c8ae
Ulli Goschler <noreply>
2022-02-04 11:40:44 +0100

    Switch arrows to vim arrows

--- keyboards/kinesis/keymaps/nce/keymap.c
+++ keyboards/kinesis/keymaps/nce/keymap.c
@@ -19,10 +19,10 @@
 * |--------+------+------+------+------+------|                           |------+------+------+------+------+--------|
 * | Shift  |   Z  |   X  |   C  |   V  |   B  |                           |   N  |   M  |  ,.  |  .>  |  /?  | Shift  |
 * `--------+------+------+------+------+-------                           `------+------+------+------+------+--------'
-*          | `~   | INS  | Left | Right|                                         | Up   | Down |  [{  |  ]}  |
+*          | `~   | INS  | Left | Down |                                         | Up   | Rght |  [{  |  ]}  |
 *          `---------------------------'                                         `---------------------------'
 *                                        ,-------------.         ,-------------.
-*                                        | CMD  | Alt  |         | PgUp  | Ctrl |
+*                                        | CMD  | Alt  |         | PgUp  | Ctrl|
 *                                 ,------|------|------|         |------+------+------.
 *                                 |      |      | Home |         | PgDn |      |      |
 *                                 | BkSp | Esc  |------|         |------|Return| Space|
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB    ,KC_Q      ,KC_W      ,KC_E      ,KC_R      ,KC_T      ,
   KC_LCTL   ,KC_A      ,KC_S      ,KC_D      ,KC_F      ,KC_G      ,
   KC_LSFT   ,KC_Z      ,KC_X      ,KC_C      ,KC_V      ,KC_B      ,
-             KC_GRV    ,KC_INS    ,KC_LEFT   ,KC_RGHT   ,
+             KC_GRV    ,KC_INS    ,KC_LEFT   ,KC_DOWN   ,
                         KC_LGUI   ,KC_LALT   ,
                         KC_HOME   ,
                         KC_BSPC   ,KC_ESC    ,KC_MINS   ,
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_Y      ,KC_U      ,KC_I      ,KC_O      ,KC_P      ,KC_BSLS   ,
   KC_H      ,KC_J      ,KC_K      ,KC_L      ,LT(SYMBOLS, KC_SCLN) ,KC_QUOT,
   KC_N      ,KC_M      ,KC_COMM   ,KC_DOT    ,KC_SLSH   ,KC_RSFT   ,
-  KC_UP     ,KC_DOWN   ,KC_LBRC   ,KC_RBRC   ,
+  KC_UP     ,KC_RGHT   ,KC_LBRC   ,KC_RBRC   ,
                         KC_PGUP   ,KC_RALT   ,
                         KC_PGDOWN ,
                         KC_EQL    ,KC_ENTER  ,KC_SPC
