2d2e2479604a8e8fae23fb567f486598f4c1bec3
Ulli Goschler <git@nce.wtf>
2024-03-06 13:23:20 +0100

    update to latest qmk; rework layout

--- keyboards/kinesis/keymaps/nce/config.h
+++ keyboards/kinesis/keymaps/nce/config.h
@@ -1,3 +1,3 @@
-#pragma once
+//#pragma once
 
 // place overrides here
--- keyboards/kinesis/keymaps/nce/keymap.c
+++ keyboards/kinesis/keymaps/nce/keymap.c
@@ -1,7 +1,23 @@
+/* Copyright 2021 Michael Stapelberg
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include QMK_KEYBOARD_H
 
 #define QWERTY 0 // Base qwerty
-#define SYMBOLS 1 // Symbols and functions
+#define LAYER1 1
 
 
 /****************************************************************************************************
@@ -15,62 +31,47 @@
 * |--------+------+------+------+------+------|                           +------+------+------+------+------+--------|
 * | Tab    |   Q  |   W  |   E  |   R  |   T  |                           |   Y  |   U  |   I  |   O  |   P  | \|     |
 * |--------+------+------+------+------+------|                           |------+------+------+------+------+--------|
-* | Ctrl   |   A  |   S  |   D  |   F  |   G  |                           |   H  |   J  |   K  |   L  |  ;:  | '"     |
+* | Caps   |   A  |   S  |   D  |   F  |   G  |                           |   H  |   J  |   K  |   L  |  ;:  | '"     |
 * |--------+------+------+------+------+------|                           |------+------+------+------+------+--------|
 * | Shift  |   Z  |   X  |   C  |   V  |   B  |                           |   N  |   M  |  ,.  |  .>  |  /?  | Shift  |
 * `--------+------+------+------+------+-------                           `------+------+------+------+------+--------'
-*          | `~   | INS  | Left | Down |                                         | Up   | Rght |  [{  |  ]}  |
+*          | `~   | INS  | Left | Right|                                         | Up   | Down |  [{  |  ]}  |
 *          `---------------------------'                                         `---------------------------'
 *                                        ,-------------.         ,-------------.
-*                                        | CMD  | Alt  |         | PgUp  | Ctrl|
+*                                        | Ctrl | Alt  |         | Gui  | Ctrl |
 *                                 ,------|------|------|         |------+------+------.
-*                                 |      |      | Home |         | PgDn |      |      |
-*                                 | BkSp | Esc  |------|         |------|Return| Space|
-*                                 |      |      | -_   |         | =+   |      |      |
+*                                 |      |      | Home |         | PgUp |      |      |
+*                                 | BkSp | Del  |------|         |------|Return| Space|
+*                                 |      |      | End  |         | PgDn |      |      |
 *                                 `--------------------'         `--------------------'
 */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [QWERTY] = LAYOUT(
-  KC_ESC    ,KC_F1     ,KC_F2     ,KC_F3     ,KC_F4     ,KC_F5     ,KC_F6  ,KC_F7  ,KC_F8,
-  KC_EQL    ,KC_1      ,KC_2      ,KC_3      ,KC_4      ,KC_5      ,
-  KC_TAB    ,KC_Q      ,KC_W      ,KC_E      ,KC_R      ,KC_T      ,
-  KC_LCTL   ,KC_A      ,KC_S      ,KC_D      ,KC_F      ,KC_G      ,
-  KC_LSFT   ,KC_Z      ,KC_X      ,KC_C      ,KC_V      ,KC_B      ,
-             KC_GRV    ,KC_INS    ,KC_LEFT   ,KC_DOWN   ,
-                        KC_LGUI   ,KC_LALT   ,
-                        KC_HOME   ,
-                        KC_BSPC   ,KC_ESC    ,KC_MINS   ,
-
-  KC_F9     ,KC_F10    ,KC_F11    ,KC_F12    ,KC_PSCR   ,KC_SLCK   ,KC_PAUS, KC_FN0, RESET,
-  KC_6      ,KC_7      ,KC_8      ,KC_9      ,KC_0      ,KC_MINS   ,
-  KC_Y      ,KC_U      ,KC_I      ,KC_O      ,KC_P      ,KC_BSLS   ,
-  KC_H      ,KC_J      ,KC_K      ,KC_L      ,LT(SYMBOLS, KC_SCLN) ,KC_QUOT,
-  KC_N      ,KC_M      ,KC_COMM   ,KC_DOT    ,KC_SLSH   ,KC_RSFT   ,
-  KC_UP     ,KC_RGHT   ,KC_LBRC   ,KC_RBRC   ,
-                        KC_PGUP   ,KC_RALT   ,
-                        KC_PGDOWN ,
-                        KC_EQL    ,KC_ENTER  ,KC_SPC
+           KC_ESC, KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,KC_F7  ,KC_F8,    KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,KC_PSCR ,KC_SCRL  ,KC_PAUS, MO(1), QK_BOOT,
+           KC_EQL, KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,     KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,KC_MINS,
+           KC_TAB, KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,     KC_Y   ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,KC_BSLS,
+           KC_LCTL,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,     KC_H   ,KC_J   ,KC_K   ,KC_L   ,LT(LAYER1, KC_SCLN),KC_QUOT, KC_LSFT,
+           KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,KC_N   ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,KC_RSFT,
+                KC_GRV ,KC_INS ,KC_LEFT,KC_DOWN,        KC_UP  ,KC_RGHT, KC_LBRC, KC_RBRC,
+			               KC_LGUI,KC_LALT,           KC_RCTL,KC_RALT,
+                                    KC_MINS,          KC_EQL,
+                           KC_BSPC,KC_ESC ,LT(LAYER1, KC_F16) ,   LT(LAYER1, KC_F17),KC_ENTER ,KC_SPC
     ),
-[SYMBOLS] = LAYOUT(
-  KC_TRNS   ,KC_TRNS   ,KC_TRNS   ,KC_TRNS   ,KC_TRNS   ,KC_TRNS   ,KC_TRNS  ,KC_TRNS  ,KC_TRNS,
-  KC_TRNS   ,KC_TRNS   ,KC_TRNS   ,KC_TRNS   ,KC_TRNS   ,KC_TRNS   ,
-  KC_TRNS   ,KC_TRNS   ,KC_TRNS   ,KC_TRNS   ,KC_TRNS   ,KC_TRNS   ,
-  KC_TRNS   ,KC_TRNS   ,KC_TRNS   ,KC_TRNS   ,KC_TRNS   ,KC_TRNS   ,
-  KC_TRNS   ,KC_TRNS   ,KC_TRNS   ,KC_TRNS   ,KC_TRNS   ,KC_TRNS   ,
-             KC_TRNS   ,KC_TRNS   ,KC_TRNS   ,KC_TRNS   ,
-                        KC_TRNS   ,KC_MPLY   ,
-                        KC_MNXT   ,
-                        KC_TRNS   ,KC_TRNS   ,KC_MPRV,
 
-  KC_TRNS   ,KC_TRNS   ,KC_TRNS   ,KC_TRNS   ,KC_TRNS   ,KC_TRNS   ,KC_TRNS, KC_TRNS, RESET,
-  KC_TRNS   ,KC_TRNS   ,KC_TRNS   ,KC_TRNS   ,KC_TRNS   ,KC_TRNS   ,
-  KC_TRNS   ,KC_TRNS   ,KC_TRNS   ,KC_TRNS   ,KC_TRNS   ,KC_TRNS   ,
-  KC_LEFT   ,KC_DOWN   ,KC_UP     ,KC_RGHT   ,KC_TRNS   ,KC_TRNS   ,
-  KC_TRNS   ,KC_TRNS   ,KC_TRNS   ,KC_TRNS   ,KC_TRNS   ,KC_TRNS   ,
-                        KC_TRNS   ,KC_TRNS   ,KC_TRNS   ,KC_TRNS   ,
-                        KC_MUTE   ,KC_TRNS   ,
-                        KC_VOLU   ,
-                        KC_VOLD   ,KC_TRNS   ,KC_TRNS
-    )
+// LAYER1 is activated by the keypad key. It has WAKE on Escape (left-most key),
+// and volume keys on PSCR (mute), SLCK (volume down) and PAUS (volume up),
+// as printed on the Kinesis Advantage 2.
+[LAYER1] = LAYOUT(
+
+           KC_WAKE,  KC_TRNS  ,KC_TRNS  ,KC_TRNS  ,KC_TRNS  ,KC_TRNS  ,KC_TRNS  ,KC_TRNS  ,KC_TRNS,    KC_TRNS  ,KC_TRNS  ,KC_TRNS  ,KC_TRNS  ,KC_MUTE ,KC_VOLD  ,KC_VOLU, KC_TRNS, KC_TRNS  ,
+           KC_TRNS  ,  KC_TRNS  ,KC_TRNS  ,KC_TRNS  ,KC_TRNS  ,KC_TRNS  ,   KC_TRNS  ,KC_TRNS  ,KC_TRNS  ,KC_TRNS  ,KC_TRNS  ,KC_TRNS  ,
+           KC_TRNS  ,  KC_TRNS  ,KC_TRNS  ,KC_TRNS  ,KC_TRNS  ,KC_TRNS  ,   KC_TRNS  ,KC_TRNS  ,KC_TRNS  ,KC_TRNS  ,KC_TRNS  ,KC_TRNS  ,
+           KC_TRNS  ,  KC_TRNS  ,KC_TRNS  ,KC_TRNS  ,KC_TRNS  ,KC_TRNS  ,   KC_LEFT  ,KC_DOWN  ,KC_UP  ,KC_RGHT  ,KC_TRNS  ,KC_TRNS  ,
+           KC_TRNS  ,  KC_TRNS  ,KC_TRNS  ,KC_TRNS  ,KC_TRNS  ,KC_TRNS  ,   KC_TRNS  ,KC_TRNS  ,KC_TRNS  ,KC_TRNS  ,KC_TRNS  ,KC_TRNS  ,
+                   KC_TRNS  ,KC_TRNS  ,KC_TRNS  ,KC_TRNS  ,             KC_TRNS  ,KC_TRNS  ,KC_TRNS  ,KC_TRNS  ,
+			   KC_TRNS  ,KC_TRNS  ,           KC_TRNS  ,KC_TRNS  ,
+                                    KC_TRNS  ,           KC_TRNS  ,
+                           KC_TRNS  ,KC_TRNS  ,KC_F18  ,           KC_F19  ,KC_TRNS    ,KC_TRNS
+    ),
 };
